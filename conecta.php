0f3c494fb742babe1eb66a58cc60bbc549602312
Douglas O.R <douglasor@outlook.com>
2017-09-24 18:31:42 -0300

    heroku

--- conecta.php
+++ conecta.php
@@ -1,11 +1,5 @@
 <?php
 
-    
-    $host = $_ENV["DATABASE_TESTHOST"];
-    $porta = $_ENV["DATABASE_PORT"];
-    $db = $_ENV["DATABASE_TESTDBNAME"];
-    $user = $_ENV["DATABASE_TESTDBUSER"];
-    $pass = $_ENV["DATABASE_TESTDBPASS"];
+    $con = $_ENV["con_string"];
 
-
- $conexao = pg_connect($host,$porta,$db, $user, $pass);
+ $conexao = pg_connect($con);
