13c7cdfd2ae99667a5fdda15f898349c58299ec1
Douglas O.R <douglasor@outlook.com>
2017-09-24 18:53:47 -0300

    heroku

--- conecta.php
+++ conecta.php
@@ -1,6 +1,5 @@
 <?php
 
-    $con = "host=ec2-23-21-220-152.compute-1.amazonaws.com
-     port=5432  dbname=dbfsj5pv00mdk0  user=lwesfasolsdlvf  password=4cc36f4e3f97e0c175a130431d5a0d0e46ea2aad7a923315f114b28a7e7aff7a";
+    $con = $_ENV["con_string"];
 
  $conexao = pg_connect($con);
