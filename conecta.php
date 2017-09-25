4c8dfdbbfdee2e27359e678b51f53d4bdee88f44
Douglas O.R <douglasor@outlook.com>
2017-09-24 21:08:52 -0300

    heroku

--- conecta.php
+++ conecta.php
@@ -1,7 +1,6 @@
 <?php
 
-$con = isset($_ENV["con_string"]) ? $_ENV["con_string"] : "host=ec2-23-21-220-152.compute-1.amazonaws.com
-port=5432  dbname=dbfsj5pv00mdk0  user=lwesfasolsdlvf  password=4cc36f4e3f97e0c175a130431d5a0d0e46ea2aad7a923315f114b28a7e7aff7a";
+$con = isset($_ENV["con_string"]) ? $_ENV["con_string"] : $_ENV["con_string"] ;
 
 
 
